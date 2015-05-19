0643c96adc0b69b13a1611d5a0027479f4ab55a5
Jami Couch <jami.couch@metova.com>
2015-05-19 09:56:39 -0500

    Lesson 2 - Buttons, Outlets, Actions

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingInProgress: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Record user voice
+        println("recordAudio")
+        recordingInProgress.hidden = false
+        stopButton.hidden = false
+        recordButton.hidden = true
+    }
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        //TODO: Stop recording
+        println("stopRecording")
+        stopButton.hidden = true
+        recordingInProgress.hidden = true
+        recordButton.hidden = false
+    }
 }
 
